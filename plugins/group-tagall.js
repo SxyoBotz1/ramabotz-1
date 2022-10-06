7421377481b21c2a875858bd72e74ff44b390d44
SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>
2022-10-06 15:45:44 +0800

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone'
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    let bcbg = 'https://telegra.ph/file/bf1dbe3da7a7023e39397.jpg'
+    let bcbg = 'https://telegra.ph/file/ad002c8965829f0ff9795.jpg'
     conn.send3ButtonLoc(m.chat, bcbg, wm, `${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────','Menu', '.menu', 'Owner', '.owner', `\nKakek Gw Sugiono`, '.huuu', null,
  /*let m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, */{
         mentions: users
