ca3357fe2589a144eae6fab376c13120a4e187b9
SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>
2022-10-06 15:53:53 +0800

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -4,18 +4,18 @@ let handler = async (m, { conn, participants }) => {
     let kickedUser = []
     for (let user of users)
         if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
             kickedUser.concat(res)
             await delay(1 * 1000)
         }
-    m.reply(`Mampos Dikick Kau ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
+    m.reply(`Succes kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
 
 }
-handler.help = ['kick'].map(v => v + ' @user')
+handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
 handler.tags = ['group']
-handler.command = /^(kick)$/i
+handler.command = /^(kick|-)$/i
 
-handler.owner = false
+handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
