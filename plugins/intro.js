e80ded9919543b782ed5569216d56cdbdc83a9fc
SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>
2022-10-15 20:46:42 +0800

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -1,1 +1,51 @@
-const _0x5e7e23=_0x22d6;function _0x27a6(){const _0x3fbab4=['340HSqTbc','search','151821trekLA','table','⚡Ping','(((.+)+)+)+$','send3ButtonDoc','20POHeSG','27343976bbBdbZ','12482SsmsWG','chat','error','__proto__','apply','toString','bind','prototype','command','3294356nDNBEm','💌\x20Creator','591PpDdaD','console','.creator','1050429txHFAD','.menu','constructor','info','{}.constructor(\x22return\x20this\x22)(\x20)','2784jfjCOh','╭──═\x20⳹°❀❬\x20*「\x20Intro\x20Card\x20」*\x20❭❀°\x0a\x0a│\x20➺\x20*Nama\x20\x20\x20\x20\x20:*\x20\x0a\x0a│\x20➺\x20*Gender\x20\x20\x20:*\x20\x0a\x0a│\x20➺\x20*Umur\x20\x20\x20\x20\x20\x20:*\x20\x0a\x0a│\x20➺\x20*Hobby\x20\x20\x20\x20:*\x20\x0a\x0a│\x20➺\x20*Kelas\x20\x20\x20\x20\x20\x20:*\x20\x0a\x0a│\x20➺\x20*Asal\x20\x20\x20\x20\x20\x20\x20\x20\x20:*\x20\x0a\x0a│\x20➺\x20*Agama\x20\x20\x20\x20:*\x20\x0a\x0a┴\x20➺\x20*Status\x20\x20\x20\x20\x20:*\x20\x0a\x0a\x20✧\x0a\x0a┬\x20\x20⌕\x20\x20▌│█║▌║▌║║▌║▌║█│▌⌕\x0a\x0a╰━━━━━━━━━━━━━┈─ɞ\x0a\x0a~>\x20Made\x20With\x20❤\x20By\x20Yanxiao\x0a\x0a','245772NjYaDU','.ping','2933IziDwN','length','exception'];_0x27a6=function(){return _0x3fbab4;};return _0x27a6();}(function(_0x258068,_0x33faad){const _0x15d740=_0x22d6,_0x3b30f8=_0x258068();while(!![]){try{const _0x7a677b=-parseInt(_0x15d740(0x146))/0x1+parseInt(_0x15d740(0x15b))/0x2*(-parseInt(_0x15d740(0x143))/0x3)+parseInt(_0x15d740(0x164))/0x4+parseInt(_0x15d740(0x159))/0x5*(parseInt(_0x15d740(0x14d))/0x6)+-parseInt(_0x15d740(0x14f))/0x7*(-parseInt(_0x15d740(0x14b))/0x8)+parseInt(_0x15d740(0x154))/0x9*(-parseInt(_0x15d740(0x152))/0xa)+parseInt(_0x15d740(0x15a))/0xb;if(_0x7a677b===_0x33faad)break;else _0x3b30f8['push'](_0x3b30f8['shift']());}catch(_0x54a8da){_0x3b30f8['push'](_0x3b30f8['shift']());}}}(_0x27a6,0xbaead));const _0x4b4373=(function(){let _0x231cc0=!![];return function(_0x2fdfcb,_0x2b6a49){const _0xc9f3ff=_0x231cc0?function(){const _0xc4cc6a=_0x22d6;if(_0x2b6a49){const _0x121c5a=_0x2b6a49[_0xc4cc6a(0x15f)](_0x2fdfcb,arguments);return _0x2b6a49=null,_0x121c5a;}}:function(){};return _0x231cc0=![],_0xc9f3ff;};}()),_0x3d4df1=_0x4b4373(this,function(){const _0x23521b=_0x22d6;return _0x3d4df1[_0x23521b(0x160)]()[_0x23521b(0x153)](_0x23521b(0x157))['toString']()[_0x23521b(0x148)](_0x3d4df1)['search'](_0x23521b(0x157));});_0x3d4df1();const _0x5e1678=(function(){let _0x440afa=!![];return function(_0x46ebae,_0x282ffa){const _0x16f67e=_0x440afa?function(){const _0x3c889c=_0x22d6;if(_0x282ffa){const _0x4e9404=_0x282ffa[_0x3c889c(0x15f)](_0x46ebae,arguments);return _0x282ffa=null,_0x4e9404;}}:function(){};return _0x440afa=![],_0x16f67e;};}()),_0x40ceb5=_0x5e1678(this,function(){const _0x4d1c91=_0x22d6,_0x1343de=function(){const _0x4620c6=_0x22d6;let _0x110dfd;try{_0x110dfd=Function('return\x20(function()\x20'+_0x4620c6(0x14a)+');')();}catch(_0x444ea9){_0x110dfd=window;}return _0x110dfd;},_0x34c44d=_0x1343de(),_0x533ebc=_0x34c44d['console']=_0x34c44d[_0x4d1c91(0x144)]||{},_0x1d5da9=['log','warn',_0x4d1c91(0x149),_0x4d1c91(0x15d),_0x4d1c91(0x151),_0x4d1c91(0x155),'trace'];for(let _0x3ef512=0x0;_0x3ef512<_0x1d5da9[_0x4d1c91(0x150)];_0x3ef512++){const _0x35e95b=_0x5e1678['constructor'][_0x4d1c91(0x162)][_0x4d1c91(0x161)](_0x5e1678),_0x4db4a9=_0x1d5da9[_0x3ef512],_0x26faa6=_0x533ebc[_0x4db4a9]||_0x35e95b;_0x35e95b[_0x4d1c91(0x15e)]=_0x5e1678[_0x4d1c91(0x161)](_0x5e1678),_0x35e95b['toString']=_0x26faa6[_0x4d1c91(0x160)][_0x4d1c91(0x161)](_0x26faa6),_0x533ebc[_0x4db4a9]=_0x35e95b;}});_0x40ceb5();import _0x1c05a8 from'node-fetch';let handler=async(_0x44f9e7,{conn:_0x5117b9,usedPrefix:_0x9432c3,command:_0x2ee221})=>{const _0x4c60fc=_0x22d6;let _0x57ec12=_0x4c60fc(0x14c);await _0x5117b9[_0x4c60fc(0x158)](_0x44f9e7[_0x4c60fc(0x15c)],_0x57ec12,author,_0x4c60fc(0x142),_0x4c60fc(0x145),'🎀\x20Menu',_0x4c60fc(0x147),_0x4c60fc(0x156),_0x4c60fc(0x14e),fpayment,adReply);};handler[_0x5e7e23(0x163)]=/^(intro)$/i;function _0x22d6(_0xfdf7f9,_0x2c3095){const _0x6192c2=_0x27a6();return _0x22d6=function(_0x40ceb5,_0x5e1678){_0x40ceb5=_0x40ceb5-0x142;let _0x5079d1=_0x6192c2[_0x40ceb5];return _0x5079d1;},_0x22d6(_0xfdf7f9,_0x2c3095);}export default handler;
+/*let handler = async m => {
+let krtu = `Kartu Intro`
+m.reply(`
+0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Kartu Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Kelas      :* 
+│ *Asal         :* 
+│ *Agama    :* 
+|  *Status     :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`.trim()) // Tambah sendiri kalo mau
+}
+handler.command = /^(intro)$/i
+export default handler */
+
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Kartu Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Kelas      :* 
+│ *Asal         :* 
+│ *Agama    :* 
+|  *Status     :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/sampun_penak",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
+    title: 'TheBotz-Official MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) // Tambah sendiri kalo mau
+}
+handler.command = /^(intro)$/i
+
+export default handler
